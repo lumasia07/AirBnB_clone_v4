8c8fab3ada8c8b7fdd713f71437d10ea55f14cb9
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-06-02 19:55:02 +0300

    web dynamic

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+    const amenitiesSelected = {};
+
+    $('input[type="checkbox"]').change(function () {
+        const amenityID = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+
+        if ($(this).is(':checked')) {
+            amenitiesSelected[amenityID] = amenityName;
+        } else {
+            delete amenitiesSelected[amenityID];
+        }
+
+        const amenitiesList = Object.values(amenitiesSelected).join(', ');
+        $('.amenities h4').text(amenitiesList);
+    });
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+        if (data.status === "OK") {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    }).fail(function () {
+        $('#api_status').removeClass('available');
+    });
+
+  function allPlaces(data) {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({}),
+      success: function (data) {
+        for (const place of data) {
+          const article = $('<article></article>');
+
+          const titlePlace = $('<div class="title_box"></div>');
+          titlePlace.append(`<h2>${place.name}</h2>`);
+          titlePlace.append(`<div class="price_by_night">$${place.price_by_night}</div>`);
+
+          const moreInfo = $('<div class="information"></div>');
+          moreInfo.append(`<div class="max_guest">${place.max_guest} Guests</div>`);
+          moreInfo.append(`<div class="number_rooms">${place.number_rooms} Bedrooms</div>`);
+          moreInfo.append(`<div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>`);
+
+          const des = $('<div class="description"></div>');
+          des.append(`<p>${place.description}</p>`);
+
+          article.append(titlePlace);
+          article.append(moreInfo);
+          article.append(des);
+
+          $('section.places').append(article);
+          }
+        },
+        error: function (error) {
+            console.log('Error:', error);
+        }
+    });
+  }
+
+  allPlaces({});
+
+$('button').click(function () {
+    allPlaces({ amenities: Object.keys(amenitiesSelected) });
+  });
+});
